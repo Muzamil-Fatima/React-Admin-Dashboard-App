1e6cfeedf0342412dd2810cbbb3c47a647b4fed5
Muzamil-Fatima <muzamilzubair987654@gmail.com>
2026-03-02 15:22:05 +0500

    Add files

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
-import './App.css';
+import { ColorModeContext, useMode } from "./theme";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import TopBar from "./scenes/global/TopBar";
+import "./App.css";
 
 function App() {
+  const [theme, colorMode] = useMode();
   return (
-    <div className="app">
-  
-    </div>
+    <ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <div className="app">
+          <main className="context">
+            <TopBar />
+          </main>
+        </div>
+      </ThemeProvider>
+    </ColorModeContext.Provider>
   );
 }
 
